fbfb1c7fdc4d5f5f8273277a037dbf02c2e47a80
Bartosz Raczkowski <bart@Bartoszs-MacBook-Air.local>
2025-02-27 13:46:31 +0100

    fix: wrong shortcut of project's name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic"
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-    title: "TOC",
+    title: "ToC",
     tagline: "Theory of Componentization",
     favicon: "img/favicon.ico",
 
@@ -62,7 +62,7 @@ const config: Config = {
         // Replace with your project's social card
         image: "img/docusaurus-social-card.jpg",
         navbar: {
-            title: "TOC",
+            title: "ToC",
             logo: {
                 alt: "Bordowe Logo",
                 src: "img/logo.svg",
